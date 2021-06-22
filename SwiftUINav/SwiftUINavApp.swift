ab156c1886a7183289949eb6979fad32c83ac0cd
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-06-22 19:16:47 +0500

    Initial Commit

---
+++ SwiftUINav/SwiftUINavApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavApp.swift
+//  SwiftUINav
+//
+//  Created by Thor on 22/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
